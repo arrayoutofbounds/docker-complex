0ee11274979a713d12b9055829eebc10de3a7f68
Anmol Desai <anmoldesai4@gmail.com>
2020-04-01 18:10:31 +1100

    setup basic multi facet app with docker

--- server/index.js
+++ server/index.js
@@ -22,4 +22,60 @@ const pgClient = new Pool({
 
 pgClient.on('error', () => console.log("Lost PG connection"))
 
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT').catch((err) => console.log(err));
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch((err) => console.log(err));
+
+// redis client setup
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+
+// duplicate connection because redis doc shows that
+// client that publishes or listens needs another connection and cannot be 
+// used for other things
+const redisPublisher = redisClient.duplicate()
+
+// express route handlers
+
+app.get('/', (req, res) => {
+    res.send("Hi")
+})
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query("SELECT * from values");
+    res.send(values.rows);
+});
+
+app.get("/values/current", async (req, res) => {
+    // return all indices and their values
+    // get all values from the hash
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    });
+})
+
+app.post("/values", async (req, res) => {
+    const index = req.body.index;
+
+    if(parseInt(index) > 40){
+        return res.status(422).send("Index too high");
+    }
+
+    // set value as nothing for the index
+    redisClient.hset('values', index, 'Nothing yet!');
+
+    // send information to publisher in worker
+    redisPublisher.publish('insert', index);
+
+    // store the value of the index in postgres
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+
+    res.send({ working: true });
+});
+
+app.listen(5000, err => {
+    console.log("Listening");
+});
